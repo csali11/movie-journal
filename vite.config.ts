37299769cf68b4ce16416968e98c58120f4d0db0
Aletta Nagy <aletta.nagy@npsh.hu>
2024-09-19 18:22:56 +0200

    Add server proxy

--- vite.config.ts
+++ vite.config.ts
@@ -5,13 +5,16 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/movie-journal/",
-  plugins: [
-    vue(),
-  ],
+  base: '/movie-journal/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    proxy: {
+      '/api': 'http://localhost:8080'
     }
   }
 })
